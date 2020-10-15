01f199c98b34a51d27affbd441ced79fbf635ed7
Lucio Franco <luciofranco14@gmail.com>
2020-10-15 10:35:28 -0400

    add client example

---
+++ examples/hyper_client.rs
@@ -0,0 +1,16 @@
+use hyper::{Client, Uri};
+use tokio_compat_02::FutureExt;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = Client::new();
+
+    // This will not panic because we are wrapping it in the
+    // Tokio 0.2 context.
+    client
+        .get(Uri::from_static("http://tokio.rs"))
+        .compat()
+        .await?;
+
+    Ok(())
+}
