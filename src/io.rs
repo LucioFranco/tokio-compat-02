f9231e010592d5ee36efbc67e7609010747a5ee2
Alice Ryhl <alice@ryhl.io>
2020-10-19 21:09:09 +0200

    Add Stream impl

--- src/io.rs
+++ src/io.rs
@@ -10,6 +10,7 @@ use tokio_02::runtime::Handle;
 use tokio::io::{
     AsyncBufRead as AsyncBufRead03, AsyncRead as AsyncRead03, AsyncWrite as AsyncWrite03, ReadBuf,
 };
+use tokio::stream::Stream;
 
 pin_project! {
     /// `IoCompat` allows conversion between the 0.2 and 0.3 IO traits.
@@ -150,5 +151,17 @@ impl<T: AsyncBufRead03> AsyncBufRead02 for IoCompat<T> {
 
     fn consume(self: Pin<&mut Self>, amt: usize) {
         self.project().inner.consume(amt)
+    }
+}
+
+impl<T: Stream> Stream for IoCompat<T> {
+    type Item = T::Item;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<T::Item>> {
+        let me = self.project();
+        let handle = me.handle;
+        let inner = me.inner;
+
+        handle.enter(|| inner.poll_next(cx))
     }
 }
